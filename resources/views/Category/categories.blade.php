0739e365c025b662f12a24c5bcdd952a19d47367
Mohammad Eftekhari <m.eftekhari@global.leaseweb.com>
2024-07-13 09:49:16 +0200

    Small refactorings in categories.blade.php

--- resources/views/Category/categories.blade.php
+++ resources/views/Category/categories.blade.php
@@ -2,48 +2,49 @@
 
 @section('content')
 
-<div class="container">
-    <div class="content">
-        <div class="row">
-        <div class="col-3" style="color: #00d084"><h3>Categories</h3></div>
-        <div class="col-9"><a href="{{route('categories.create')}}"><button  style="background-color: #00d084" class="btn ">Create</button></a></div>
-    </div>
-    </div>
-<table class="table table-light table-hover"  >
-    <thead>
-    <tr>
-        <th scope="col">Title</th>
-        <th scope="col">Icon</th>
-        <th scope="col">Action</th>
-
-    </tr>
-    </thead>
-    <tbody>
-    <tr>@foreach ($categories as $category)
-        <td>{{$category->title}}</td>
-        <td><div>{{$category->icon}}</div> <div><i class="bi bi-{{$category->icon}}"></i></div> </td>
-        <td>
+    <div class="container">
+        <div class="content">
             <div class="row">
-                <div class="col-2"></div>
-                    <div class="col-2"></div>
-                <div class="col-4"> <form action="{{route('categories.destroy',$category->id)}}" method="post">
-                        @csrf
-                        {{method_field('DELETE')}}
-                        <a href="{{route('categories.destroy',$category->id)}}"> <button class="btn btn-danger">delete</button></a>
-                    </form></div>
-                <div class="col-4"><a href="{{route('categories.edit',$category->id)}}"><button class="btn btn-warning" >edit</button>  </a></div>
-
+                <div class="col-3" style="color: #00d084"><h3>Categories</h3></div>
+                <div class="col-9">
+                    <a href="{{route('categories.create')}}">
+                        <button style="background-color: #00d084" class="btn ">Create</button>
+                    </a>
+                </div>
             </div>
-
-        </td>
-
-
-
-    </tr>
-
-
-    @endforeach
-    </tbody>
-</table>
-</div>
+        </div>
+        <table class="table table-light table-hover">
+            <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Icon</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>@foreach ($categories as $category)
+                    <td>{{$category->title}}</td>
+                    <td>
+                        <span style="padding-right: 5px;">
+                            <i class="bi bi-{{$category->icon}}"></i>
+                        </span>
+                        <span>{{$category->icon}}</span>
+                    </td>
+                    <td>
+                        <a class="btn btn-warning" href="{{route('categories.edit',$category->id)}}">
+                            Edit
+                        </a>
+                        <form action="{{route('categories.destroy',$category->id)}}" method="post" style="display: inline-block;">
+                            @csrf
+                            {{method_field('DELETE')}}
+                            <button class="btn btn-danger">
+                                Delete
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
