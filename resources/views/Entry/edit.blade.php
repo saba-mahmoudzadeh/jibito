0803da4a0e7bc10fe2bca8423d1c4b774369e6e0
unknown <you@example.com>
2024-07-23 20:11:21 +0330

    Fix old for edit page

--- resources/views/Entry/edit.blade.php
+++ resources/views/Entry/edit.blade.php
@@ -25,7 +25,7 @@
                     <label for="title">Title</label>
                 </div>
                 <div class="col-9">
-                    <input name="title" type="text" value="{{$entries->title}}" class="form-control" >
+                    <input name="title" type="text" value="{{old('title',$entries->title)}}" class="form-control" >
                 </div>
             </div>
 
@@ -34,7 +34,7 @@
                     <label for="text">Amount</label>
                 </div>
                 <div class="col-9">
-                    <input name="amount" type="text" class="form-control" value="{{$entries->amount}}">
+                    <input name="amount" type="text" class="form-control" value="{{old('amount',$entries->amount)}}">
                 </div>
             </div>
 
@@ -43,12 +43,12 @@
                 </div>
                 <div class="col-9">
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="type" id="inlineRadio1" value="income" {{ $entries->type == 'income' ? 'checked' : '' }}>
+                        <input class="form-check-input" type="radio" name="type" id="inlineRadio1" value="income"  @if(old('type',$entries->type)=='income')checked @endif   >
                         <label class="form-check-label" for="inlineRadio1">Income</label>
                     </div>
 
                     <div class="form-check form-check-inline" style="margin-top: 10px; margin-bottom: 10px">
-                        <input class="form-check-input" type="radio" name="type" id="inlineRadio2" value="expense" {{ $entries->type == 'expense' ? 'checked' : '' }}>
+                        <input class="form-check-input" type="radio" name="type" id="inlineRadio2" value="expense" @if(old('type',$entries->type)=='expense')checked @endif>
                         <label class="form-check-label" for="inlineRadio2">Expense</label>
                     </div>
                 </div>
@@ -59,7 +59,7 @@
                     <label for="entry_date">Date</label>
                 </div>
                 <div class="col-9">
-                    <input name="entry_date" type="text" class="form-control" id="datepicker" width="230" value="{{$entries->entry_date}}">
+                    <input name="entry_date" type="text" class="form-control" id="datepicker" width="230" value="{{old('entry_date',$entries->entry_date)}}">
                 </div>
             </div>
 
@@ -69,9 +69,9 @@
                 </div>
                 <div class="col-9">
                     <select class="form-select form-control" name="category_id" id="specificSizeSelect">
-                        <option class="form-control" value="">Choose...</option>
+                        <option class="form-control" value="" @if(old('category_id',$entries->category_id)== '')selected @endif>>Choose...</option>
                         @foreach($categories as $category)
-                            <option value="{{ $category->id }}" {{ $entries->category_id == $category->id ? 'selected' : '' }}>
+                            <option value="{{ $category->id }}" @if(old('category_id',$entries->category_id)== $category->id)selected @endif>
                                 {{ $category->title }}
                             </option>
                         @endforeach
@@ -85,7 +85,7 @@
                 </div>
                 <div class="col-9">
                     <div class="input-group">
-                        <textarea  name="description" class="form-control" style="padding-left: 30px" aria-label="With textarea" >{{$entries->description}}</textarea>
+                        <textarea  name="description" class="form-control" style="padding-left: 30px" aria-label="With textarea" >{{old('description',$entries->description)}}</textarea>
                     </div>
                 </div>
             </div>
