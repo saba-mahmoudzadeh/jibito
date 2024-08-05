bdf9648625608a2b73aeb3a5afc00e16c7a25a93
unknown <you@example.com>
2024-08-05 21:24:14 +0330

    Add athentication and laravel breez

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,21 @@ use App\Http\Controllers\EntryController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/',[IndexController::class,'index'])->name('home.index');
 Route::get('/categories',[CategoryController::class,'index'])->name('categories.index');
